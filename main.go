63e09de49b9d99c3c6d302850cad26cb5a27b83b
scgitstuff <67354101+scgitstuff@users.noreply.github.com>
2025-12-06 17:12:48 -0500

    1.3

--- main.go
+++ main.go
@@ -1,9 +1,43 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
+const MSG_File = "messages.txt"
+
 func main() {
-	fmt.Println("I hope I get the job!")
+	readMessages(MSG_File)
+}
+
+func failOnErr(err error, msg string) {
+	if err != nil {
+		// panic(err)
+		log.Fatal(msg)
+	}
+}
+
+func readMessages(fileName string) {
+	fmt.Printf("Reading data from %s\n", fileName)
+	fmt.Println("=====================================")
+
+	f, err := os.Open(MSG_File)
+	failOnErr(err, fmt.Sprintf("failed to open file: %s", MSG_File))
+	defer f.Close()
+
+	buf := make([]byte, 8)
+	for {
+		n, err := f.Read(buf)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			failOnErr(err, fmt.Sprintf("Read failed:\n%v\n", err))
+		}
+		fmt.Printf("read: %s\n", buf[:n])
+	}
 }
