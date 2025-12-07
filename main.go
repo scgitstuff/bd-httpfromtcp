44b962ab2b15ab2cc74c66041909bd9ef9f8b42a
scgitstuff <67354101+scgitstuff@users.noreply.github.com>
2025-12-07 16:24:52 -0500

    1.5

--- main.go
+++ main.go
@@ -12,7 +12,16 @@ import (
 const MSG_File = "messages.txt"
 
 func main() {
-	readLines(MSG_File)
+	// readLines(MSG_File)
+	_ = readLines
+
+	f, err := os.Open(MSG_File)
+	failOnErr(err, fmt.Sprintf("failed to open file: %s", MSG_File))
+	defer f.Close()
+	ch := getLinesChannel(f)
+	for line := range ch {
+		fmt.Printf("read: %s\n", line)
+	}
 }
 
 func failOnErr(err error, msg string) {
@@ -52,4 +61,39 @@ func readLines(fileName string) {
 			continue
 		}
 	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+
+	ch := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(ch)
+		buf := make([]byte, 8)
+		line := ""
+		for {
+			n, err := f.Read(buf)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					if line != "" {
+						ch <- line
+					}
+					break
+				}
+				failOnErr(err, fmt.Sprintf("Read failed:\n%v\n", err))
+			}
+			line += string(buf[:n])
+
+			lines := strings.Split(line, "\n")
+			if len(lines) > 1 {
+				for i := 0; i < len(lines)-1; i++ {
+					ch <- lines[i]
+				}
+				line = lines[len(lines)-1]
+			}
+		}
+	}()
+
+	return ch
 }
