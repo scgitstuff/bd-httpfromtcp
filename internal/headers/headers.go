d8dccad905de7b483484a830ed7fd3df246c0825
scgitstuff <67354101+scgitstuff@users.noreply.github.com>
2025-12-17 18:46:58 -0500

    5.2

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,6 +3,7 @@ package headers
 import (
 	"bytes"
 	"fmt"
+	"slices"
 	"strings"
 )
 
@@ -32,14 +33,50 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 		return 0, false, fmt.Errorf("':' char not found")
 	}
 
-	if key[len(key)-1:] == " " {
+	if key[len(key)-1] == ' ' {
 		return 0, false, fmt.Errorf("invalid header name: %s", key)
 	}
-
 	key = strings.TrimSpace(key)
+	key = strings.ToLower(key)
+	if !isValidKey([]byte(key)) {
+		return 0, false, fmt.Errorf("invalid key: %s", key)
+	}
+
 	value = strings.TrimSpace(value)
 
-	h[key] = value
+	h.Set(key, value)
 
 	return n, false, nil
+}
+
+func (h Headers) Set(key, value string) {
+	key = strings.ToLower(key)
+	h[key] = value
+}
+
+func (h Headers) Get(key string) string {
+	key = strings.ToLower(key)
+	return h[key]
+}
+
+func isValidKey(key []byte) bool {
+	for _, c := range key {
+		if c >= 'a' && c <= 'z' {
+			continue
+		}
+
+		if isValidSymbol(c) {
+			continue
+		}
+
+		return false
+	}
+
+	return true
+}
+
+func isValidSymbol(c byte) bool {
+	symbols := []byte{'!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~'}
+
+	return slices.Contains(symbols, c)
 }
